d98d8e30bf3e5452f8a9530bee2185d06c63765f
Kulinych <kulinych@gmail.com>
2022-08-23 16:34:51 +0300

    Create parsing.py

---
+++ parsing.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+from bs4 import BeautifulSoup as bs
+import requests
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+TOKEN = "Tokenbot"
+chat_id = "ID chat for send message"
+keysearch ="one key"
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+wd = webdriver.Chrome(options=options)
+wd.get("https://kufar.by/l")
+wd.implicitly_wait(20)
+
+search_input = wd.find_element(By.CSS_SELECTOR, '.styles_input_rightIcon__vrNqG')
+search_input.send_keys(f"{keysearch} + '\ue007'")
+time.sleep(9)
+
+soup = bs(wd.page_source, "html.parser")
+soupfind = soup.find("a", class_="styles_wrapper__pb4qU")
+link = soupfind["href"]
+names = soup.find('div', class_="styles_left___v6uP")
+file = names.text + link
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={file}"
+f = open('./test.txt', 'r')
+t = f.read()
+if t != file:
+    requests.post(url)
+    with open('./test.txt', 'w') as f:
+      f.write(file)
+      f.close()
+ 
