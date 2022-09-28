c558065eb632250ee2c6111c1f63113fbaa14ce5
SergeyK <kulinych@gmail.com>
2022-09-28 13:53:44 +0300

    Update parcing tag

--- parsing.py
+++ parsing.py
@@ -24,15 +24,14 @@ options.add_argument('--disable-dev-shm-usage')
 wd = webdriver.Chrome(options=options)
 wd.get(f"https://www.kufar.by/l?ot=1&query={keysearch}&rgn=all&utm_search=Query%20only")
 wd.implicitly_wait(20)
-wd.close
+
 
 soup = bs(wd.page_source, "html.parser")
-soupfind = soup.find("a", class_="styles_wrapper__pb4qU")
+soupfind = soup.find("a", class_="styles_wrapper__2ZGa_")
 link = soupfind["href"]
-names = soup.find("h3", class_="styles_title__wj__Y")
-price = soup.find("p", class_="styles_price__x_wGw")
-#file = names.text +  price.text + link
-
+names = soup.find("h3")
+price = soup.find("p", class_="styles_price__Bdda5")
+wd.close()
 file = f'Объявление: {names.text}, Цена: {price.text}, Ссылка: {link}'
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={file}"
@@ -41,5 +40,4 @@ if t != file:
     requests.post(url)
     with open('./test.txt', 'w') as f:
       f.write(file)
-      f.close()
 #print(file)
