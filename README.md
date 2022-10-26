6ca3e44c19a02d6766c898b10d00635ff242e6ec
SergeyK <kulinych@gmail.com>
2022-10-26 09:42:33 +0300

    Add all photo in Ad

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Parsing Kufar.by  
 This script send notice telegram channel when new AD in keysearch on kufar.by 
 
+Edit TOKEN and Chat_id on you.  
+
+Run console pip install -r requirements.txt
+
 Change file on executable chmod +x parsing.py
 
 Edit crontab -e
--- parsing.py
+++ parsing.py
@@ -4,42 +4,60 @@ import requests
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import telegram
+from telegram import InputMediaPhoto
 
-TOKEN = "Tokenbot"
-chat_id = "ID chat for send message"
-keysearch ="сноуборд"
 
+
+TOKEN = "token"
+chat_id = "chat_id"
+keysearch = "сноуборд"
+photos = []
+bot = telegram.Bot(token=TOKEN)
 try:
-  f = open('./test.txt', 'r')
+    f = open('./test.txt', 'r')
 except:
-  f = open('./test.txt', 'x')
-  f.close()
+    f = open('./test.txt', 'x')
+    f.close()
 finally:
-  f = open('./test.txt', 'r')
-  t = f.read()
+    f = open('./test.txt', 'r')
+    t = f.read()
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 wd = webdriver.Chrome(options=options)
-wd.get(f"https://www.kufar.by/l?ot=1&query={keysearch}&rgn=all&utm_search=Query%20only")
+wd.get(f"https://www.kufar.by/l?query={keysearch}")
 wd.implicitly_wait(20)
-
 
 soup = bs(wd.page_source, "html.parser")
 soupfind = soup.find("a", class_="styles_wrapper__pb4qU")
 link = soupfind["href"]
+link_photo = wd.get(link)
 names = soup.find("h3")
 price = soup.find("p", class_="styles_price__x_wGw")
-img_url = soup.find("img", class_="styles_image--blur__6MsOZ lazyload")["data-src"]
+
+soup_photo = bs(wd.page_source, "html.parser")
+soup_find_all_photo = soup_photo.findAll("img", class_="styles_slide__image__lc2v_")
+for photo in soup_find_all_photo:
+    if photo["src"] not in photos:
+        photos.append(photo["src"])
 wd.close()
+
+media_group = []
+
+for number, url in enumerate(photos):
+    media_group.append(InputMediaPhoto(media=url, caption=names.text + price.text))
+
+
 file = f'Объявление: {names.text}, Цена: {price.text}, Ссылка: {link}'
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={file}"
-photo = f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_id}&caption={file}&photo={img_url}'
+# message = f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_id}&caption={file}&photo={photos}'
 
 if t != file:
-    requests.post(photo)
+    bot.send_media_group(chat_id=chat_id, media=media_group)
+    requests.post(url)
     with open('./test.txt', 'w') as f:
       f.write(file)
-#print(file)
+
