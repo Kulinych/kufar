6e4e3c87931b3d5b26f88900c4145b398f219cb9
SergeyK <kulinych@gmail.com>
2023-01-12 00:09:28 +0300

    delete webdriver

--- parsing.py
+++ parsing.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from bs4 import BeautifulSoup as bs
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from argparse import ArgumentParser  
 import telegram
 from telegram import InputMediaPhoto
@@ -15,16 +13,17 @@ def get_photo(link):
   # парсинг фотографий и описания
   photo_link = []
   description = 'Описание не найдено'
-  wd.get(link)
+  a = requests.get(link)
   try:
-    soup_photo = bs(wd.page_source, "html.parser")
+    soup_photo = bs(a.content, "html.parser")
     soup_find_all_photo = soup_photo.findAll("img", class_="styles_slide__image__lc2v_", limit=9) 
     for photo in soup_find_all_photo:
       if photo["src"] not in photo_link:
-        photo_link.append(photo["src"])
+        photo_link.append(photo["src"])      
     description = soup_photo.find("div", itemprop="description").text
     if len(description) > 870:
       description = description[:867] + "..." 
+    
   except: pass
   return photo_link, description       
 
@@ -38,12 +37,13 @@ def get_api():
       if int(ads['price_byn']) != 0:
         price = int(ads['price_byn']) / 100
       else: price = "Договорная"
+    return link, names, price
   except: pass
-  return link, names, price
+  
 
 def main():
   media_group = []
-  # Проверка наличия файла. 
+  # Проверка наличия файла (костыль). 
   try: f = open(keysearch + ".txt", 'r')
   except:
     f = open(keysearch + ".txt", 'x')
@@ -52,25 +52,28 @@ def main():
     f = open(keysearch + ".txt", 'r')
     t = f.read()
   
-  link, names, price = get_api()
-  file = f'Объявление: {names}, Цена: {price} '
-
-  if get_photo(link) != None: 
-    for number, url in enumerate(get_photo(link)[0]):
-      if number == 0:
-        media_group.append(InputMediaPhoto(media=url, parse_mode='HTML', caption=file + "<a href='" + link + "'>Ссылка</a>, Описание: " + get_photo(link)[1]))
-      media_group.append(InputMediaPhoto(media=url))
-  wd.quit()
-
+  try:
+    link, names, price = get_api()
+    file = f'Объявление: {names}, Цена: {price} '
+  #a = requests.get(link)
+  #print(a.content)
+    if get_photo(link) != None: 
+      for number, url in enumerate(get_photo(link)[0]):
+        if number == 0:
+          media_group.append(InputMediaPhoto(media=url, parse_mode='HTML', caption=file + "<a href='" + link + "'>Ссылка</a>, Описание: " + get_photo(link)[1]))
+        media_group.append(InputMediaPhoto(media=url))
+  
+  
   # Запись в файл для сравнения. отправка сообщения 
-  if t != file:
-      with open(keysearch + ".txt", 'w') as f:
-        f.write(file)
-        f.close()
-      if len(media_group) != 0:
-        bot.send_media_group(chat_id=chat_id, media=media_group)  
-      else: bot.send_message(text=file + link, chat_id=chat_id)
-
+    if t != file:
+        with open(keysearch + ".txt", 'w') as f:
+          f.write(file)
+          f.close()
+        if len(media_group) != 0:
+          bot.send_media_group(chat_id=chat_id, media=media_group)  
+        else: bot.send_message(text=file + link, chat_id=chat_id)
+  except: print('Объявлений не найдено')
+    
 if __name__ == "__main__":
   ap = ArgumentParser()
   ap.add_argument('-t', '--TOKEN', default=TOKEN, type = str, help='nput token bot, 46 symbols')
@@ -80,14 +83,6 @@ if __name__ == "__main__":
 
   chat_id = a.chat_id
   keysearch = a.search
-  
-  # Загрузка webdriver с Chrome и дополнительными опциями
-  options = webdriver.ChromeOptions()
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--incognito')
-  wd = webdriver.Chrome(options=options)
-
+    
   bot = telegram.Bot(token=a.TOKEN)
   main()
